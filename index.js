ca313122de28831c99c5cf352774c6a73dbf4cbb
Olubimi Michael <Olubimimichael01@gmail.com>
2022-11-11 14:43:19 +0000

    some corrections

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const app = express()
 
 app.use(express.json())
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 const MONGO_URI =
-  "mongodb+srv://solomon:solomontest@cluster0.4cupn.mongodb.net/todolist?retryWrites=true&w=majority"
+  "mongodb+srv://Hollumidhe:Hollumidhe12@cluster0.2ticcn3.mongodb.net/?retryWrites=true&w=majority"
 
 app.use('/todos', todo)
 
 mongoose.connect(MONGO_URI).then((res) => {
-  if(res) {
+  if (res) {
     console.log('Mongodb Connected')
   }
-    app.listen(3000, () => {console.log('Server Listening on port 3000')})
+  app.listen(3000, () => { console.log('Server Listening on port 8000') })
 })
